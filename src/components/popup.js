58a876a6f560f6d3c10faaf94b39aa0ae3b2ddc9
Ayush Bansal <ayu023ban@gmail.com>
2020-10-23 12:09:55 +0530

    - Make generic confirm modal - make generic popupView - add delete folder functionality in tabular view

--- src/components/popup.js
+++ src/components/popup.js
@@ -7,196 +7,68 @@ import {
   deleteFile,
   unsetSelected,
   fetchFiles,
-  editFile
+  editFile,
 } from '../actions/index'
+import PropTypes from 'prop-types'
+
 import EditModal from './edit-modal'
 
 import index from './css/index.css'
 import popup from './css/popup.css'
+import ConfirmModal from './confirmModal'
 class PopupView extends Component {
   constructor(props) {
     super(props)
     this.state = {
       isPopupOpen: false,
-      showModal: false,
-      fileName: '',
-      isPublic: false,
-      isDelete: false
     }
     this.contextRef = React.createRef()
   }
-  componentDidUpdate(prevProps) {
-    const { selectedData } = this.props
-    if (
-      JSON.stringify(prevProps.selectedData) !== JSON.stringify(selectedData)
-    ) {
-      this.setState({
-        fileName: selectedData.fileName,
-        isPublic: selectedData.isPublic
-      })
-    }
-  }
-  handleEdit = () => {
-    this.setState(
-      {
-        showModal: true
-      },
-      this.handlePopupToggle(null, false)
-    )
-  }
-  successCallback = () => {
-    this.setState({
-      showModal: false
-    })
-  }
-  close = () => {
-    this.setState({
-      showModal: false
-    })
-  }
   handlePopupToggle = (e, value) => {
     this.setState({
-      isPopupOpen: value
+      isPopupOpen: value,
     })
-    if (e && e.type === 'click') e.stopPropagation()
-  }
-  handleDownload = () => {
-    const { isSelected, selectedData } = this.props
-    if (isSelected) {
-      let link = document.createElement('a')
-      link.download = selectedData.fileName
-      link.href = selectedData.link
-      document.body.appendChild(link)
-      link.click()
-      document.body.removeChild(link)
-    }
-    this.handlePopupToggle(null, false)
-  }
-  handleDelete = () => {
-    const {
-      isSelected,
-      selectedData,
-      deleteFile,
-      unsetSelected,
-      id
-    } = this.props
-    isSelected && selectedData.pk === id
-      ? deleteFile(selectedData.pk, this.successCallback)
-      : deleteFile(id, this.successCallback)
-    unsetSelected()
-    this.handlePopupToggle(null, false)
-  }
-  successCallback = () => {
-    this.props.fetchFiles()
-  }
-  handleClick = (pk, fileName, link, index, isPublic) => {
-    const { setSelected } = this.props
-    this.setState({
-      active: index
-    })
-    setSelected({ pk, fileName, link, isPublic })
-  }
-  handleSelect = e => {
     if (e && e.type === 'click') {
-      this.props.handleClick()
       e.stopPropagation()
+      e.preventDefault()
     }
   }
-  showDeleteModal = () => {
-    this.setState({
-      isDelete: true
-    })
-  }
-  closeDeleteModal = () => {
-    this.setState({
-      isDelete: false
-    })
-  }
+
   render() {
-    const { isPopupOpen, showModal, fileName, isDelete } = this.state
-    const { isSelected } = this.props
+    const { isPopupOpen } = this.state
+    const { options, handleOptions, ...props } = this.props
     return (
       <React.Fragment>
         <Popup
           trigger={
-            <Button icon floated="right" onClick={e => this.handleSelect(e)}>
+            <Button icon floated="right">
               <Icon name="ellipsis horizontal" />
             </Button>
           }
           open={isPopupOpen}
           onClose={e => this.handlePopupToggle(e, false)}
           onOpen={e => this.handlePopupToggle(e, true)}
-          onClick={e => this.handleSelect(e)}
           wide
           flowing
-          hoverable
+          on="click"
           position="left center"
           styleName="popup.padding-edit"
+          {...props}
         >
-          {/* <div styleName="popup.button-group">
-            <div styleName="popup.button-padding">
-              <Button
-                labelPosition="left"
-                icon
-                onClick={this.handleEdit}
-                color="green"
-              >
-                <Icon name="edit" />
-                Edit
-              </Button>
-            </div>
-            <div styleName="popup.button-padding">
-              <Button
-                labelPosition="left"
-                secondary
-                onClick={this.handleDownload}
-                icon
-              >
-                <Icon name="download" />
-                Download
-              </Button>
-            </div>
-            <div styleName="popup.button-padding">
-              <Button
-                color="red"
-                labelPosition="left"
-                onClick={this.showDeleteModal}
-                icon
-              >
-                <Icon name="delete" />
-                Delete
-              </Button>
-            </div>
-          </div> */}
           <Menu vertical>
-            <Menu.Item name="edit" onClick={this.handleEdit} />
-            <Menu.Item name="download" onClick={this.handleDownload} />
-            <Menu.Item name="delete" onClick={this.showDeleteModal} />
+            {options.map(option => (
+              <Menu.Item
+                key={option.key}
+                name={option.label}
+                icon={option.icon ? option.icon : false}
+                onClick={() => {
+                  this.setState({ isPopupOpen: false })
+                  handleOptions[option.key]()
+                }}
+              />
+            ))}
           </Menu>
         </Popup>
-        <EditModal close={this.close} showModal={showModal} />
-        {isDelete && isSelected ? (
-          <Modal size="large" open={isDelete} onClose={this.closeDeleteModal}>
-            <Modal.Header>
-              Do you really want to delete "{fileName}"
-            </Modal.Header>
-            <Modal.Content>
-              <p>Are you sure you want to delete this file?</p>
-            </Modal.Content>
-            <Modal.Actions>
-              <Button negative onClick={this.closeDeleteModal}>
-                No
-              </Button>
-              <Button
-                positive
-                icon="checkmark"
-                labelPosition="right"
-                content="Yes"
-                onClick={this.handleDelete}
-              />
-            </Modal.Actions>
-          </Modal>
-        ) : null}
       </React.Fragment>
     )
   }
@@ -208,34 +80,18 @@ const mapStateToProps = state => {
     progress: state.files.progressArray,
     tabular: state.files.tabular,
     isSelected: state.files.isSelected,
-    selectedData: state.files.selectedData
+    selectedData: state.files.selectedData,
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    fetchFiles: () => {
-      dispatch(fetchFiles())
-    },
-    setSelected: data => {
-      dispatch(setSelected(data))
-    },
-    setTarget: () => {
-      dispatch(setTarget())
-    },
-    deleteFile: (pk, callback) => {
-      dispatch(deleteFile(pk, callback))
-    },
-    unsetSelected: () => {
-      dispatch(unsetSelected())
-    },
-    editFile: (pk, data, callback) => {
-      return dispatch(editFile(pk, data, callback))
-    }
-  }
+PopupView.propTypes = {
+  options: PropTypes.array,
+  handleOptions: PropTypes.object,
+  onClick: PropTypes.func,
 }
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(PopupView)
+PopupView.defaultProps = {
+  options: [],
+  handleOptions: {},
+  onClick: () => {},
+}
+export default connect(mapStateToProps)(PopupView)
