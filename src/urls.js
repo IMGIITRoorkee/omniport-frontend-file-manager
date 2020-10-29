57c7a22507f7c2028200927f670d95aa3692da8f
gauransh7 <gauransh7dingwani@gmail.com>
2020-10-30 03:24:36 +0530

    Remove needless urls

--- src/urls.js
+++ src/urls.js
@@ -1,36 +1,4 @@
 // Backend
-// export function urlBase () {
-//   return `/api/django_filemanager/`
-// }
-
-// export function urlFilesList () {
-//   return `${urlBase()}folder`
-// }
-
-// // export function urlUploadFile () {
-// //   return `${urlBase()}upload/`
-// // }
-
-// export function urlDeleteFile (pk) {
-//   return `${urlBase()}delete_file/${pk}/`
-// }
-
-// export function urlEditFile (pk) {
-//   return `${urlBase()}edit_file/${pk}/`
-// }
-
-// export function urlFolderFiles (pk) {
-//   return `${urlBase()}folder/${pk}/files/`
-// }
-
-// export function urlFile (pk) {
-//   return `files/${pk}/`
-// }
-
-// export function urlUploadFile () {
-//   return `files/`
-// }
-
 export const FILE_APIS = {
   fileItem: '/files'
 }
