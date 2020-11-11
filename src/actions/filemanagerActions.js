22fc26dc62ec81f6f46940b068d9f953fe3904c8
gauransh7 <gauransh7dingwani@gmail.com>
2020-11-12 04:58:34 +0530

    Create separate filemanager actions/reducers, declare constants, use redux state

---
+++ src/actions/filemanagerActions.js
@@ -0,0 +1,37 @@
+import {
+  CREATE_FILEMANAGER_PENDING,
+  FILEMANAGER_API_ERROR
+} from './filemanagerActionTypes'
+import { FILEMANAGER_APIS } from '../urls'
+import apiClient from '../helpers/apiClient'
+
+const apiDispatch = (actionType = '', data) => {
+  return {
+    type: actionType,
+    payload: data
+  }
+}
+
+const apiError = error => {
+  return {
+    type: FILEMANAGER_API_ERROR,
+    error
+  }
+}
+
+export const createFilemanager = (data, callback) => {
+  const url = `${FILEMANAGER_APIS.filemanagerItem}/`
+  return dispatch => {
+    dispatch(apiDispatch(CREATE_FILEMANAGER_PENDING, true))
+    apiClient
+      .post(url, data)
+      .then(res => {
+        dispatch(apiDispatch(CREATE_FILEMANAGER_PENDING, false))
+        callback()
+      })
+      .catch(error => {
+        dispatch(apiDispatch(CREATE_FILEMANAGER_PENDING, false))
+        dispatch(apiError(error))
+      })
+  }
+}
