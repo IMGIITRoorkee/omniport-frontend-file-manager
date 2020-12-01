a72230b9ce60482bfa8735e02f33f9fc746b378b
Ayush Bansal <ayu023ban@gmail.com>
2020-12-01 11:16:30 +0530

    Make right click functionality in tabular view

--- src/helpers/helperfunctions.js
+++ src/helpers/helperfunctions.js
@@ -39,3 +39,22 @@ export const formatStorage = storage => {
   }
   return formattedStr
 }
+
+export const createContextFromEvent = e => {
+  const left = e.clientX
+  const top = e.clientY
+  const right = left + 1
+  const bottom = top + 1
+
+  return {
+    getBoundingClientRect: () => ({
+      left,
+      top,
+      right,
+      bottom,
+
+      height: 0,
+      width: 0
+    })
+  }
+}
